ff614f9a8e32c494ea15405794d22a9f643e0699
Hassine ALLAYA <allayahassine@gmail.com>
2018-12-23 22:55:25 +0100

    project setup

---
+++ StudentInformationSystem-Back/src/main/java/com/hassine/SisApplication.java
@@ -0,0 +1,14 @@
+package com.hassine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisApplication.class, args);
+	}
+
+}
+
